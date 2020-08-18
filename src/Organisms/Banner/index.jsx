8788bb427a65646538f427444e7328e7c2b41f4a
Sviatoslav.V <sviatoslav.v.itlt@gmail.com>
2020-08-18 18:10:16 +0300

    banner done

---
+++ src/Organisms/Banner/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles.module.scss'
+import { Container} from 'react-bootstrap';
+
+const Banner = () => {
+    return (
+        <Container fluid className={styles.banner}>
+
+        </Container>
+    )
+}
+
+export default Banner
--- src/Organisms/index.js
+++ src/Organisms/index.js
@@ -3,3 +3,5 @@ export { default as MiddleHeader } from './Middle-header/index';
 export { default as DarkFooter } from './Footer-dark/index';
 export { default as FooterPartners } from './Footer-partners';
 export { default as Copyright } from './Copyright';
+export { default as Banner } from './Banner';
+
